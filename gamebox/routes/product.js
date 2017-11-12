de83e0b2cd8d9248a639801e7d04a7117fac605c
GUEST\tinnguyen <nctin0802@gmail.com>
2017-11-12 19:27:47 +0700

    done_basic View, Add, Update, Delete

--- gamebox/routes/product.js
+++ gamebox/routes/product.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bcrypt = require('../libraries/bcrypt_module');
 var constant = require('../libraries/constant_module');
-var _status = require('../libraries/constant_status');
+var file = require('../libraries/file_module');
 var router = express.Router();
+
+const IMAGE_PATH = './public/images/product/';
 
 // Models
 var Product = require('../models/product');
@@ -12,7 +14,6 @@ router.get('/', (req, res) => {
         res.render('admin/product/', {
             page_title: 'List of Games',
             products: products,
-            status_labels: _status.getStatusLabels(),
         });
     });
 });
@@ -23,7 +24,6 @@ router.get('/view/:id', (req, res, next) => {
         res.render('admin/product/view', {
             page_title: product.name,
             product: product,
-            status_labels: _status.getStatusLabels(),
         });
     });
 });
@@ -31,19 +31,24 @@ router.get('/view/:id', (req, res, next) => {
 router.all('/add', (req, res, next) => {
     if (req.method == 'POST') {
         var data_post = req.body;
+        if (data_post.image) {
+            var buffer = file.decodeBase64(data_post.image);
+            if (buffer) {
+                data_post.image = 'product_' + buffer.name;
+                file.upload(IMAGE_PATH + data_post.image, buffer.data);
+            }
+        }
         Product.insert(data_post).then(result => {
             if (!result) {
                 req.session.message = constant.getErrorMessage('The product is already in the data.');
                 res.redirect('/admin/product');
             } else {
-                // Post image
                 res.redirect('/admin/product/view/' + result.insertedIds[0]);
             }
         });
     } else {
         res.render('admin/product/form', {
             page_title: 'Add New Product',
-            status_options: _status.getStatusOptions(),
             product: {},
         });
     }
@@ -54,37 +59,47 @@ router.all('/update/:id', (req, res, next) => {
     Product.findOne(condition).then(product => {
         if (req.method == 'POST') {
             var data_post = req.body;
-            console.log(data_post);
+            data_post.updated_at = new Date().getTime();
+            if (product.image != data_post.image) {
+                var buffer = file.decodeBase64(data_post.image);
+                if (buffer) {
+                    data_post.image = 'product_' + buffer.name;
+                    file.upload(IMAGE_PATH + data_post.image, buffer.data);
+                    file.remove(IMAGE_PATH + product.image);
+                }
+            }
             Product.update(condition, data_post).then(result => {
                 if (!result) {
-                    req.session.message = _status.getErrorMessage('The product is already in the data.');
+                    req.session.message = constant.getErrorMessage('The product is already in the data.');
                 } else {
-                    // Post image
-                    req.session.message = _status.getSuccessMessage(data_post.name + ' was updated successfully.');
+                    req.session.message = constant.getSuccessMessage(data_post.name + ' was updated successfully.');
                 }
                 res.redirect('/admin/product');
             });
         } else {
-            console.log(_status.getStatusOptions());
             res.render('admin/product/form', {
                 page_title: 'Update Product: ' + product.name,
                 product: product,
-                status_options: _status.getStatusOptions(),
             });
         }
     });
 });
 
 router.post('/delete', (req, res, next) => {
-    // var condition = { _id: Product.getObjectId(req.body.id) };
-    // Product.delete(condition).then(result => {
-    //     if (!result) {
-    //         req.session.message = constant.getErrorMessage('Delete user failed.');
-    //     } else {
-    //         req.session.message = constant.getSuccessMessage('Delete user successfully.');
-    //     }
-    //     res.redirect('/admin/product');
-    // });
+    var condition = { _id: Product.getObjectId(req.body.id) };
+    Product.findOne(condition).then(product => {
+        if (product) {
+            file.remove(IMAGE_PATH + product.image);
+        }
+    });
+    Product.delete(condition).then(result => {
+        if (!result) {
+            req.session.message = constant.getErrorMessage('Delete product failed.');
+        } else {
+            req.session.message = constant.getSuccessMessage('Delete product successfully.');
+        }
+        res.redirect('/admin/product');
+    });
 });
 
 // catch 404 page not found
